86eaf36e77af6854609137cf881aba031465d9a7
Yin Lee <leesrrh@gmail.com>
2024-07-13 18:10:07 +0900

    init commit

---
+++ src/test/java/tyhyun/fortune/FortuneApplicationTests.java
@@ -0,0 +1,13 @@
+package tyhyun.fortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
